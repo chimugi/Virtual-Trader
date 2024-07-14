f32c20fa62dd9215c94ca57066c2e97bac9591d5
tsuru <ttt.04139200@gmail.com>
2024-07-15 02:02:35 +0900

    remove table component

--- src/app/virtual-trader/contract.ts
+++ src/app/virtual-trader/contract.ts
@@ -1,11 +1,9 @@
 import { VirtualTraderFormComponent } from './virtual-trader-form/virtual-trader-form.component';
 import { VirtualTraderGraphComponent } from './virtual-trader-graph/virtual-trader-graph.component';
-import { VirtualTraderTableComponent } from './virtual-trader-table/virtual-trader-table.component';
 
 export const VIRTUAL_TRADER_COMPONENTS = [
 	VirtualTraderFormComponent,
 	VirtualTraderGraphComponent,
-	VirtualTraderTableComponent,
 ];
 
 export interface StockTicker {
@@ -17,9 +15,7 @@ export interface StockTicker {
 }
 
 export interface HistoricalData {
-	// Define the structure of your historical data here
-	date: string; // Example, adjust according to your API response
-	close: number; // Example
+	date: string;
+	close: number;
 	code: string;
-	// Add other properties as needed
 }
